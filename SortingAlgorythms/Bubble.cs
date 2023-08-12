d8add1970dc42179352581e41f44cf3882e3f685
Karen Dashyan <dashyankaren@outlook.com>
2023-08-12 07:38:36 +0400

    sorting algorythms

--- SortingAlgorythms/Bubble.cs
+++ SortingAlgorythms/Bubble.cs
@@ -1,28 +1,23 @@
-﻿using System;
-
-public class BubbleSort
+﻿public class BubbleSort
 {
-	public BubbleSort()
-	{
-	}
-	public int[] Sort(int[] arr)
-	{
-		int temp;
-		for (int i = 0; i < arr.Length; i++)
-		{
-			for ( int j = 0; j < arr.Length-1; j++)
-			{
-				if (arr[j] > arr[j+1])
-				{
-					temp = arr[j];
-					arr[j] = arr[j+1];
-					arr[j+1] = temp;
-				}
-			}
-		}
-
-
-		return arr;
-	
-	}
+    public BubbleSort()
+    {
+    }
+    public int[] Sort(int[] arr)
+    {
+        int temp;
+        for (int i = 0; i < arr.Length; i++)
+        {
+            for (int j = 0; j < arr.Length - 1; j++)
+            {
+                if (arr[j] > arr[j + 1])
+                {
+                    temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        return arr;
+    }
 }
